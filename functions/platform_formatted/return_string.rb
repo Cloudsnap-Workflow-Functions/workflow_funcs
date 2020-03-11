c963048838df8cd5810fea7c98f68c5f6f9438a0
SirWilliamIII <willcarpenterwastaken@gmail.com>
2020-03-10 21:11:03 -0500

    created folder structure

---
+++ functions/platform_formatted/return_string.rb
@@ -0,0 +1,8 @@
+# Takes one string as param and returns string  
+
+# @param string a - the string to be returned
+
+
+do |s:""|
+    return { "s:string": s }
+end
---
+++ functions/ruby_formatted/return_string.rb
@@ -0,0 +1,13 @@
+# Takes one string as param and returns string  
+
+# @param string a - the string to be returned
+
+
+def return_string(a)
+    return a
+end
+
+
+# Print function output to console
+
+p return_string("test")
